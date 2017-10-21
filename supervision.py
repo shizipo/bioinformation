887eec2205dad2af378fd1fe6e701d294129b11c
yangxinchao <30661269+yangxinchao@users.noreply.github.com>
2017-10-21 19:11:28 +0800

    just a test

--- supervision.py
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env python
-import time,os,sys
-i = 0
-while i >= 1000:
-	i += 0
-	cmd = "vjob |grep 'P101SC17040270-01_niechi'|awk '{print $0}' > 1.txt"
-	os.system(cmd)
-        time.sleep(15) 
